b5527052f083c38e971013bf68c8ba3b834671c3
Erica Dong <erica3280cell@gmail.com>
2022-08-16 17:07:05 -0400

    Add output generation for NeuralNetwork class

--- src/NeuralNetwork.java
+++ src/NeuralNetwork.java
@@ -1,14 +1,31 @@
 import java.util.*;
 
 public class NeuralNetwork {
-    ArrayList<Layer> layers;
-    int activationType;
+    Layer[] layers;
 
-    public NeuralNetwork(ArrayList<Layer> layers, int a) {
-        this.layers = layers;
-        activationType = a;
-        for (Layer l : this.layers) {
-            l.activationType = activationType;
+    public NeuralNetwork(int[] layerSizes, int activation) {
+        layers = new Layer[layerSizes.length-1]; //output "layer" isn't a true layer
+        for (int i = 0; i < layers.length; i++) {
+            layers[i] = new Layer(layerSizes[i], layerSizes[i+1]);
+            layers[i].activationType = activation;
         }
+    }
+
+    public double[] getOutputs(double[] inputs) {
+        for (Layer l : layers) {
+            inputs = l.getOutputs(inputs);
+        }
+        return inputs;
+    }
+
+    public int maxOutputIndex(double[] inputs) {
+        double[] outputs = getOutputs(inputs);
+        int index = 0;
+        for (int i = 1; i < outputs.length; i++) {
+            if (inputs[i] > inputs[index]) {
+                index = i;
+            }
+        }
+        return index;
     }
 }
